b201d5200e956e24cd683c142b1eb86767fcc55f
Alex Berezhnoy <berezhnoy20025@gmail.com>
2023-02-20 09:31:50 +0200

    Vuejs basic setup

---
+++ src/views/SecondView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import DisplayMsgComponent from '../components/DisplayMsgComponent.vue'
+</script>
+
+<template>
+  <main>
+    <DisplayMsgComponent msg="Hello from SecondView" />
+  </main>
+</template>
